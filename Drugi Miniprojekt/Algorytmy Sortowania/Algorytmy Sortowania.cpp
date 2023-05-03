d957028f65ff27a19eec31b03790bfcf7997eeb7
Yetisio <jakub.piekarek@wp.pl>
2023-05-03 18:03:10 +0200

    zmiany 03.05

--- Drugi Miniprojekt/Algorytmy Sortowania/Algorytmy Sortowania.cpp
+++ Drugi Miniprojekt/Algorytmy Sortowania/Algorytmy Sortowania.cpp
@@ -8,6 +8,9 @@
 #include "introSort.h"
 #include "mergeSort.h"
 #include "quickSort.h"
+#include "introSort.cpp"
+#include "mergeSort.cpp"
+#include "quickSort.cpp"
 
 
 using namespace std;
@@ -26,13 +29,7 @@ struct movie {
 };
 
 int main(int argc, char** argv) {
-    if (argc > 1 && argv[1] != string("--zapisz")) {
-        cout << "\nWywolanie: ./projekt2 [--zapisz limit]\n\n"
-            << "Opcje:\n"
-            << "--zapisz | limit - maksymalna ilosc danych do zapisu\n\n";
-        return 0;
-    }
-
+   
     // pliki
     ifstream dataset;  // baza filmow (wczytywany plik)
     string dataset_name = "dane2.csv";
@@ -44,24 +41,22 @@ int main(int argc, char** argv) {
     char identifier;         // , lub "
 
     // sortowania
-    std::string sorts[3] = { "merge", "quick", "intro" };
+    string sorts[3] = { "merge", "quick", "intro" };
 
     // testy
     unsigned int test = 0;    // numer testu
     unsigned int nlimit = 0;  // limit n dla zapisu na sorted_dataset
-    unsigned int ntab[] = { 10000, 100000, 500000, 1000000, 1010292 };
+    unsigned int ntab[] = { 10000, 100000, 500000, 1000000, 1010294 };
     unsigned int unrated, median;  // ilosc nieocenionych filmow, mediana ocen
     float average;                 // srednia ocen
 
-    if (argc >= 3)
-        nlimit = stoi(argv[2]);
-
+    
     cout << fixed << setprecision(3) << "\x1B[2J\x1B[H";  // precyzja wyswietlania + clear screen
 
-    for (unsigned int n : ntab)  // #include <iterator>
+    for (unsigned int n : ntab)  
     {
         test++;
-        cout << "TEST " << test << "/" << size(ntab) << ": n = " << n << '\n';
+        cout << "Proba nr " << test << "/" << size(ntab) << ": n = " << n << '\n';
         movie* parsed_dataset = new movie[n];
 
         // wczytywanie pliku
@@ -69,7 +64,7 @@ int main(int argc, char** argv) {
         dataset.open(dataset_name);
 
         if (!dataset.is_open()) {
-            cout << "Nie udalo sie otworzyc pliku " + dataset_name;
+            cout << "Problem z plikiem " + dataset_name;
             exit(1);
         }
 
@@ -80,16 +75,15 @@ int main(int argc, char** argv) {
             stringstream split(line);
             // wczytywanie indeksu i przecinka
             getline(split, temp, ',');
-            // wczytywanie nazwy - pomiedzy " " lub zwykla oddzielona
-            // przecinkiem
+            // wczytywanie nazwy - pomiedzy " " lub zwykla oddzielona przecinkiem
             stringstream::pos_type pos = split.tellg();
             split >> identifier;
             if (identifier == '"') {
                 getline(split, parsed_dataset[i].name, '"');
-                split >> identifier;  // ignoruj ,
+                split >> identifier;  // nie bierz pod uwage 
             }
             else {
-                split.seekg(pos);  // wroc
+                split.seekg(pos);  // powrot
                 getline(split, parsed_dataset[i].name, ',');
             }
             // wczytywanie oceny
@@ -99,7 +93,7 @@ int main(int argc, char** argv) {
             }
             // koniec pliku - zmiana n do liczby wczytanych filmow
             if (dataset.eof()) {
-                cout << "Plik zawiera za malo danych - wczytane filmy: " << i << " / " << n << '\n';
+                cout << "Za malo danych w podanym pliku - wczytane dane filomowe: " << i << " / " << n << '\n';
                 n = i + 1;
             }
         }
@@ -107,19 +101,19 @@ int main(int argc, char** argv) {
 
         // filtrowanie - usuwanie filmow bez ocen
         if (unrated > 0) {
-            cout << "> Filmy bez oceny: " << unrated << '\n';
-            pressEnter();
-            cout << "Filtrowanie danych\n";
+            cout << " \n Filmy nie posiadajce oceny: " << unrated << '\n';
+            
+            cout << "\nFiltracja danych z pliku\n";
         }
         else
-            cout << "> Wszystkie filmy sa ocenione.\n";
-        movie* filtered_dataset = new movie[n - unrated];
+            cout << "  \nWszystkie rekordy pliku posiadaja ocene filmu\n";
+        movie* filter_dataset = new movie[n - unrated];
         unsigned int i = 0, j = 0;
 
         auto duration_start = chrono::high_resolution_clock::now();  // mierzenie czasu filtrowania
         while (j < n)
             if (parsed_dataset[j].rating != -1)
-                filtered_dataset[i++] = parsed_dataset[j++];
+                filter_dataset[i++] = parsed_dataset[j++];
             else
                 j++;
         auto duration_end = chrono::high_resolution_clock::now();  // koniec mierzenia
@@ -128,94 +122,69 @@ int main(int argc, char** argv) {
         delete[] parsed_dataset;
 
         if (unrated > 0)
-            cout << "> Po przefiltrowaniu: " << n - unrated << '\n'
-            << "> Czas filtrowania:   " << duration << " ms\n";
+            cout << "  Rekordow po filtracji: " << n - unrated << '\n'
+            << "  Czas filtracji wzgledem oceny:   " << duration << " ms\n";
 
-        pressEnter();
+       // pressEnter();
 
         // struktury do przechowywania wynikow sortowan
-        movie** sorting_datasets = new movie * [size(sorts)];
+        movie** sort_dataset = new movie * [size(sorts)];
         for (unsigned int i = 0; i < size(sorts); ++i)
-            sorting_datasets[i] = new movie[n - unrated];
+            sort_dataset[i] = new movie[n - unrated];
         for (unsigned int i = 0; i < size(sorts); ++i)
             for (unsigned int j = 0; j < n - unrated; ++j)
-                sorting_datasets[i][j] = filtered_dataset[j];
-        delete[] filtered_dataset;
+                sort_dataset[i][j] = filter_dataset[j];
+        delete[] filter_dataset;
 
         // sortowania
         for (unsigned int i = 0; i < size(sorts); ++i) {
-            cout << "Sortowanie ";
+            cout << "\nSortowanie ";
             if (sorts[i] == "merge") {
-                cout << "przez scalanie (mergesort)\n";
+                cout << "- mergesort\n";
                 duration_start = chrono::high_resolution_clock::now();
-                mergeSort(sorting_datasets[i], 0, n - unrated - 1);
+                mergeSort(sort_dataset[i], 0, n - unrated - 1);
                 duration_end = chrono::high_resolution_clock::now();
             }
             else if (sorts[i] == "quick") {
-                cout << "szybkie (quicksort)\n";
+                cout << "- quicksort\n";
                 duration_start = chrono::high_resolution_clock::now();
-                quickSort(sorting_datasets[i], 0, n - unrated - 1);
+                quickSort(sort_dataset[i], 0, n - unrated - 1);
                 duration_end = chrono::high_resolution_clock::now();
             }
             else if (sorts[i] == "intro") {
-                cout << "introspektywne (introsort)\n";
+                cout << "- introsort\n";
                 duration_start = chrono::high_resolution_clock::now();
-                hybridIntroSort(sorting_datasets[i], 0, n - unrated - 1);
+                hybridIntroSort(sort_dataset[i], 0, n - unrated - 1);
                 duration_end = chrono::high_resolution_clock::now();
             }
             else {
-                cout << "nie powiodlo sie - brak interfejsow sortowan.\n";
+                cout << "Wskazane bledne sortowanie\n";
                 break;
             }
             duration = chrono::duration<double, milli>(duration_end - duration_start).count();
-            cout << "> Czas sortowania: " << duration << " ms\n";
+            cout << "  Czas sortowania: " << duration << " ms\n";
 
             // srednia ocen
             average = 0;
             for (unsigned int j = 0; j < n - unrated; ++j)
-                average += sorting_datasets[i][j].rating;  // average jako suma
+                average += sort_dataset[i][j].rating;  // average jako suma
             average /= n - unrated;
-            cout << "> Srednia ocen:    " << average << '\n';
+            cout << "  Srednia ocen filmow dla danego sortowania:    " << average << '\n';
 
             // mediana
             median = 0;
             if ((n - unrated) % 2 != 0)
-                median = sorting_datasets[i][(n - unrated) / 2].rating;
+                median = sort_dataset[i][(n - unrated) / 2].rating;
             else
-                median = (sorting_datasets[i][(n - unrated - 1) / 2].rating + sorting_datasets[i][(n - unrated) / 2].rating) / 2;
-            cout << "> Mediana ocen:    " << median << "\n\n";
+                median = (sort_dataset[i][(n - unrated - 1) / 2].rating + sort_dataset[i][(n - unrated) / 2].rating) / 2;
+            cout << "  Mediana ocen filmow dla danego sortowania:    " << median << "\n";
         }
 
-        // zapisywanie plikow
-        if (nlimit > 0 && nlimit >= n) {
-            cout << "Zapisywanie plikow wynikowych...\n";
-            sorted_dataset_name =
-                dataset_name.substr(0, dataset_name.length() - 4) + "_" +
-                to_string(n);
-            for (unsigned int i = 0; i < size(sorts); ++i) {
-                sorted_dataset.open(sorted_dataset_name + "_" + sorts[i] +
-                    "sort.csv");
-                if (sorted_dataset.is_open()) {
-                    sorted_dataset << ",movie,rating\n";  // header
-                    for (unsigned int j = 0; j < n - unrated; ++j)
-                        sorted_dataset << j << ",\""
-                        << sorting_datasets[i][j].name << "\","
-                        << sorting_datasets[i][j].rating << '\n';
-                    sorted_dataset.close();
-                    cout << "[ok] ";
-                }
-                else
-                    cout << "[x] ";
-                cout << sorted_dataset_name + "_" + sorts[i] +
-                    "sort.csv\n";
-                delete[] sorting_datasets[i];
-            }
-            cout << '\n';
-        }
+        
 
-        cout << "KONIEC TESTU " << test << "/" << size(ntab) << '\n';
+        cout << "\nKoniec proby " << test << "/" << size(ntab) << '\n';
         pressEnter();
-        cout << "\x1B[2J\x1B[H"; // clear screen
+        cout << "\x1B[2J\x1B[H"; 
         
     }
 }
--- Drugi Miniprojekt/Algorytmy Sortowania/quickSort.h
+++ Drugi Miniprojekt/Algorytmy Sortowania/quickSort.h
@@ -1,29 +1,7 @@
 #pragma once
 
 template <typename T>
-int partition(T* array, int start, int end) {
-    T temp, pivot = array[(start + end) / 2];  // element srodkowy
-    int l = start, r = end;                    // iteratory dla podtablic lewej i prawej
-    while (1) {
-        while (array[l] < pivot) l++;  // dopoki po lewej sa mniejsze od srodkowego
-        while (array[r] > pivot) r--;  // dopoki po prawej sa wieksze od srodkowego
-
-        if (l >= r)
-            return r;  // element osiowy
-
-        // zamien elementy - mniejszy na lewo, wiekszy na prawo
-        temp = array[l];
-        array[l++] = array[r];
-        array[r--] = temp;
-    }
-}
+int partition(T* array, int start, int end);
 
 template <typename T>
-void quickSort(T* array, int start, int end) {
-    // rekurencyjne dzielenie na 2 podtablice az do uzyskania jednoelementowych
-    if (start >= 0 && start < end) {
-        int pivot = partition(array, start, end);  // element osiowy
-        quickSort(array, start, pivot);            // lewa podtablica
-        quickSort(array, pivot + 1, end);          // prawa podtablica
-    }
-}
+void quickSort(T* array, int start, int end);
